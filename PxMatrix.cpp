f412b756aa8fc6913dd983ca5e44bf8975f65c74
Dominic Buchstaller <dom@Dominics-MBP.fritz.box>
2018-03-31 20:50:22 +0200

    typos

--- PxMatrix.cpp
+++ PxMatrix.cpp
@@ -1,7 +1,5 @@
 /*********************************************************************
-This is a library for Chinese P10 32x16 LED matrix displays
-
-It is based on the 5110 library by Adafruit
+This is a library for Chinese LED matrix displays
 
 Written by Dominic Buchstaller.
 BSD license, check license.txt for more information
