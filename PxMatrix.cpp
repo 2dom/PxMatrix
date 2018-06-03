e1ac13eefe349057fef3ff59db79a2df78712dbd
Dominic Buchstaller <dom@Dominics-MBP.fritz.box>
2018-06-03 22:49:15 +0200

    header only

--- PxMatrix.cpp
+++
@@ -1,8 +0,0 @@
-/*********************************************************************
-This is a library for Chinese LED matrix displays
-
-Written by Dominic Buchstaller.
-BSD license, check license.txt for more information
-*********************************************************************/
-
-//#include <Wire.h>
