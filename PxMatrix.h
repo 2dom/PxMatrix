cdbf7e0cf557d774434fd0a96d778f9552ea1683
Dominic Buchstaller <dbuchstaller@web.de>
2019-01-20 23:01:33 +0100

    row pattern init killed

--- PxMatrix.h
+++ PxMatrix.h
@@ -288,7 +288,6 @@ inline void PxMATRIX::init(uint8_t width, uint8_t height,uint8_t LATCH, uint8_t 
   _rotate=0;
   _fast_update=0;
 
-  _row_pattern=BINARY;
   _scan_pattern=LINE;
   _driver_chip=SHIFT;
 
